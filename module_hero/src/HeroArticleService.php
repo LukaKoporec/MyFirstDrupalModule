9328a7825732ab9bce89eaa0e580a54471250220
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-21 11:51:21 +0100

    custom service

---
+++ module_hero/src/HeroArticleService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\module_hero;
+
+/**
+ * Our hero article service class
+ */
+
+class HeroArticleService{
+
+    /**
+     * Method for getting articles, regarding heroes.
+     */
+    public function getHeroArticles() {
+        $articles = ['Hulk is green!', 'Flash is red!'];
+
+        return $articles;
+    }
+
+
+}
