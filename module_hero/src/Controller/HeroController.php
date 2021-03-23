439aec2d18094762b145b6b81b2d1c3560ba378b
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-23 12:27:07 +0100

    custom configuration

--- module_hero/src/Controller/HeroController.php
+++ module_hero/src/Controller/HeroController.php
@@ -5,29 +5,33 @@ namespace Drupal\module_hero\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\module_hero\HeroArticleService;
+use Drupal\Core\Config\ConfigFactory;
 
 /** This is our hero controller */
 
 class HeroController extends ControllerBase {
 
     private $articleHeroService;
+    protected $configFactory;
 
     public static function create(ContainerInterface $container){
         return new static(
-            $container->get('module_hero.hero_articles')
+            $container->get('module_hero.hero_articles'),
+            $container->get('config.factory')
 
         );
 
     }
 
-    public function __construct(HeroArticleService $articleHeroService) {
+    public function __construct(HeroArticleService $articleHeroService, ConfigFactory $configFactory) {
         $this->articleHeroService = $articleHeroService;
+        $this->configFactory = $configFactory;
 
     }
 
     public function heroList () {
 
-        kint($this->articleHeroService->getHeroArticles()); die();
+        
 
         $heroes = [
             ['name' => 'Hulk'],
@@ -42,7 +46,7 @@ class HeroController extends ControllerBase {
         return [
             '#theme' => 'hero_list',
             '#items' => $heroes, 
-            '#title' => $this->t('Our wonderful heroes list'),
+            '#title' => $this->configFactory->get('module_hero.settings')->get('hero_list_title'),
         ];
 
     }
